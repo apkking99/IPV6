e5af2bd9793fce6fdf5d38e9b69a3a480e41cfcb
apkking99 <95641364+apkking99@users.noreply.github.com>
2021-12-08 13:45:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-COUNT=50
+COUNT=200
 FIRST_PORT=20000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
