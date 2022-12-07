eb75ba38d8e239216c497bbb497889c63e0957c5
apkking99 <95641364+apkking99@users.noreply.github.com>
2022-12-07 19:25:34 +0530

    Create p2p.sh

---
+++ p2p.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://get.docker.com -o get-docker.sh;
+DRY_RUN=1 sudo sh ./get-docker.sh;
+ export P2P_EMAIL=<your_email_without_quotes>; 
+docker rm -f peer2profit || true && docker run -d --restart always \
+        -e P2P_EMAIL=$P2P_EMAIL \
+        --name peer2profit \
+        peer2profit/peer2profit_linux:latest
